743df2bbfff82dd0de005f4683342169b90c028a
sapphiremoonlight <mikaelsonandrea31@gmail.com>
2025-12-05 21:39:19 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -8,6 +8,7 @@ const urlsToCache = [
   '/icon.png',
 ];
 
+// Install event: cache all the assets
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME)
@@ -15,9 +16,27 @@ self.addEventListener('install', (event) => {
         console.log('Opened cache');
         return cache.addAll(urlsToCache);
       })
+      .catch((err) => console.error('Failed to cache:', err))
   );
 });
 
+// Activate event: remove old caches
+self.addEventListener('activate', (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => 
+      Promise.all(
+        cacheNames.map((cacheName) => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            return caches.delete(cacheName); // Delete old caches
+          }
+        })
+      )
+    )
+  );
+});
+
+// Fetch event: respond with cached content or fetch from network
 self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
